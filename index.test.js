f5b31dd7fe16c85f3cbc38d9bdee644b85abb303
Etienne GIROT <egirot@octo.com>
2017-10-26 18:31:13 +0200

    Triangulated behaviour

--- index.test.js
+++ index.test.js
@@ -2,10 +2,15 @@ test("1x1 board => X won", () => {
 
     // Arrange
     const oneByOneBoard = "X";
+    const boardSize = 1;
     let message = '';
 
     // Production
-    if (oneByOneBoard.includes("X")) {
+    let winningPattern = '';
+    for (let i = 0; i < boardSize; i++) {
+        winningPattern += "X";
+    }
+    if (oneByOneBoard.includes(winningPattern)) {
         message = getXWonMessage()
     }
     // Act
@@ -19,10 +24,15 @@ test("2x2 board => X won", () => {
 
     // Arrange
     const twoByTwoBoard = "XX|0 ";
+    const boardSize = 2;
     let message = '';
 
     // Production
-    if (twoByTwoBoard.includes("XX")) {
+    let winningPattern = '';
+    for (let i = 0; i < boardSize; i++) {
+        winningPattern += "X";
+    }
+    if (twoByTwoBoard.includes(winningPattern)) {
         message = getXWonMessage()
     }
     // Act
@@ -40,10 +50,15 @@ test('3 x 3 board and 3 X in lines => X won', () => {
 
     // Arrange
     const threeByThreeBoard = 'XXX|00 |   ';
+    const boardSize = 3;
     let message = '';
 
     // Production
-    if (threeByThreeBoard.includes("XXX")) {
+    let winningPattern = '';
+    for (let i = 0; i < boardSize; i++) {
+        winningPattern += "X";
+    }
+    if (threeByThreeBoard.includes(winningPattern)) {
         message = getXWonMessage()
     }
 
